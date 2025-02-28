742fac22c6282ab9b5fef736aff93f54c5ba61f1
Emily Q. Wang <ewang1@oberlin.edu>
2025-02-28 12:15:19 -0500

    Lecture 12 vowel.py demo complete

--- vowel.py
+++ vowel.py
@@ -3,38 +3,41 @@
 
 # TODO: Let's make a set of tests!
 def testVowel():
-	# print statement # Expected value 
-	print(isVowel("red")) # ??
-	print(isVowel("w")) # ??
-	print(isVowel("o")) # ??
-	print(isVowel(2)) # ??
-	return
+  # print statement # Expected value 
+  # Add more examples here!
+  print("a", isVowel("a")) # True 
+  print("i", isVowel("i")) # True 
+  print("c", isVowel("c")) # False
+  print(2, isVowel(2)) # False
+  print("?", isVowel("?")) # False
+  return
 
 # Test each option!
 # A
 def isVowel(char):
-	for letter in "aeiou":
-		if letter == char:
-			return True
-		else:
-			return False
+  for letter in "aeiou":
+    if letter == char:
+      return True
+    else:
+      return False
 
 print("testing A:")
 testVowel()
 
 # B
 def isVowel(char):
-	for letter in "aeiou":
-		if letter == char:
-			return True
-	return False
+  for letter in "aeiou":
+    if letter == char:
+      return True
+  
+  return False
 
 print("testing B:")
 testVowel()
 
 # C
 def isVowel(char):
-	return char == "a" or "e" or "i" or "o" or "u"
+  return char == ("a" or "e" or "i" or "o" or "u")
 
 print("testing C:")
 testVowel()
--- vowel_2.py
+++ vowel_2.py
@@ -3,38 +3,46 @@
 
 # TODO: Let's make a set of tests!
 def testVowel():
-	# print statement # Expected value 
-	print(isVowel("red")) # ??
-	print(isVowel("w")) # ??
-	print(isVowel("o")) # ??
-	print(isVowel(2)) # ??
-	return
+  # print statement # Expected value 
+  # Add more examples here!
+  print("a", isVowel("a")) # True
+  print("o", isVowel("o")) # True
+  print("z", isVowel("z")) # False
+  print("m", isVowel("m")) # False
+  print(5, isVowel(5)) # False
+  print("aeiou", isVowel("aeiou")) # True ? (pending)
+  return
 
-# Test each option!
-# A
-def isVowel(char):
-	for letter in "aeiou":
-		if letter == char:
-			return True
-		else:
-			return False
+# # Test each option!
+# # A
+# def isVowel(char):
+# 	for letter in "aeiou":
+# 		if letter == char:
+# 			return True
+# 		else:
+# 			return False
+#   # idea: assign T/F to a variable, return the variable at the end
 
-print("testing A:")
-testVowel()
+# print("testing A:")
+# testVowel()
 
-# B
-def isVowel(char):
-	for letter in "aeiou":
-		if letter == char:
-			return True
-	return False
+# # B
+# def isVowel(char):
+# 	for letter in "aeiou":
+# 		if letter == char:
+# 			return True
+#   return False
 
-print("testing B:")
-testVowel()
+# print("testing B:")
+# testVowel()
 
 # C
 def isVowel(char):
-	return char == "a" or "e" or "i" or "o" or "u"
+  # originally
+  # return char == "a" or "e" or "i" or "o" or "u"
+
+  # revised
+  return char == "a" or char == "e" or char == "i" or char == "o" or char == "u"
 
 print("testing C:")
 testVowel()
