9646b2e0eb6a56ec1c6c70448ef5cac7dcedd857
Emily Q. Wang <ewang1@oberlin.edu>
2025-03-05 20:12:14 -0500

    added comments

--- worm.py
+++ worm.py
@@ -1,3 +1,7 @@
+# Lecture 14: Strings
+# Activity 14.5
+# There are multiple correct answers for each of these prompts. If you have a different idea than the one shared in class, please explore it!
+
 worm = '_/\__/\__0>'
 
 # '0>'
