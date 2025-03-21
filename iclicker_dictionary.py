4d0dfa6a8fb323387c264979ef9f99857fbe91c3
Emily Q. Wang <ewang1@oberlin.edu>
2025-03-21 12:21:24 -0400

    most_recent(slist) function design, testing, & application demo complete; initial notes from setting up local IDEs with alpha testers

--- iclicker_dictionary.py
+++ iclicker_dictionary.py
@@ -4,32 +4,63 @@ iClicker = {
   'Mika': [0, 1, 1, 1, 1] # attended 1 session ago
  }
 
-def most_recent(slist):
+def most_recent_while(slist):
   """
   Given a student's list of iClicker data, return the index ('# sessions ago') of their most recent attendance.
   """
 
   # while loop approach
-  # WORK IN PROGRESS
-  value = 0
-  index = 0
-  while value == 0:
+  value = 0 # attendance value
+  index = 0 # day corresponding to attendance value
+  while value == 0: # keep going when there are absences (0s)
     value = slist[index]
-    index += 1
-  return 
+    index += 1 # check the next day
+  return index - 1 # undo the most recent index +=1
+
+def most_recent_for(slist):
+  """
+  Given a student's list of iClicker data, return the index ('# sessions ago') of their most recent attendance.
+  """
 
   # for loop approach
-  # WORK IN PROGRESS
-  for name in slist:
-    if ??? == 0:
-      days += 1
-    if ?? == 1:
-      return days
+  for index in range(len(slist)):
+    if slist[index] == 1:
+      return index 
+
+def most_recent(slist):
+  return most_recent_for(slist)
+
+def main():
+  # print(most_recent_for(iClicker['Alex']))
+  # print(most_recent_for(iClicker['Erin']))
+  # print(most_recent_for(iClicker['Mika']))
+
+  for stu in iClicker.keys():
+    # sessions_ago = ??
+    # CLICKER Q: How do I call our most_recent() function from elsewhere in the program? 
+    # A. sessions_ago = iClicker.most_recent(stu)
+    # B. sessions_ago = most_recent(stu)
+    sessions_ago = most_recent(iClicker[stu])
+    # D. sessions_ago = most_recent(iClicker, stu)
+    # E. None of the above
+
+    print(stu + " attended " + str(sessions_ago) + " sessions ago!")
+
+main()
+
+# Discarded Drafts
+  # looping by element
+  # days = 0
+  # days_present = []
+  # for attendance in slist:
+  #   if attendance == 0:
+  #     days += 1
+  #   if attendance == 1:
+  #     days += 1
+  #     days_present.append(days)
+  #     return days
 
   # pseudocode for reference
   # loop through indices
     # find 1 value
     # return current index
-
-
-print(most_recent('Erin'))
---
+++ ide.md
@@ -0,0 +1,21 @@
+# IDE
+- Install Visual Studio Code
+
+# Python
+- Install Python extension
+- Install Python interpreter (see the link from the setup checklist / webpage instructions / Windows Store)
+- Install Homebrew (webpage) (mac)
+- Try a hello.py, save a new file, run it in VS Code terminal
+
+# git & GitHub 
+- https://code.visualstudio.com/docs/sourcecontrol/github
+- Install GitHub Pull Requests extension
+- Download git with brew (webpage) or the Windows .exe
+- Click the source control section
+- Log in to GitHub (Clone Repository --> Pop-up with remote sources --> Drop down with your account repos)
+- Log in with the user account avatar, click the GitHub option
+- Clone the current lab repository
+- Try another hello.py, save a new file, run it in VS Code terminal
+- Test commit and sync
+- Proceed with care
+- Remember to pull/rebase from your online work if you switch between platforms
