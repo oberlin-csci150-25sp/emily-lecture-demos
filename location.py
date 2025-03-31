f0d15fc046cfd45948f1cde3cea077ba9c5b3e66
Emily Q. Wang <ewang1@oberlin.edu>
2025-03-31 17:46:10 -0400

    responses to location.py OOP Part 1 coding demo

--- location.py
+++ location.py
@@ -1,42 +1,27 @@
-# ITERATION 0
-class Location:
-	"""
-	Location class for representing buildings, etc.
-	"""
-	def __init__(self):
-		"""Constructor for Location class."""
-		self.x = 0
-		self.y = 0
-		self.name = ""
-	
-
-# 22.2 clicker question
-s = Location()
-s.name = "Severance"
-s.x = 5
-k = Location()
-k.name = "King"
-p = "Peters"
-
-print(k.name + " is at " + str(k.x) + ", " + str(k.y))
-
-
 # ITERATION 1+
 class Location:
-	"""
-	Location class for representing buildings, etc.
-	"""
-	def __init__(self, x, y, n):
-		"""Constructor for Location class."""
-		self.x = x
-		self.y = y
-		self.name = n	
-		
-    # 22.4 Activity: Add method, attributes, etc. based on class discussion
-	# def ...
+  """
+  Location class for representing buildings, etc.
+  """
+  def __init__(self, x, y, n):
+    """Constructor for Location class."""
+    self.x = x
+    self.y = y
+    self.name = n
+
+    # location box (e.g., property perimeter)
+    self.box = [] # list of coordinates
+
+    # ratings
+    self.ratings = [] # nested list of rating text and numbers [["review", 5]]
+    
+   # 22.4 Activity: Add method, attributes, etc. based on class discussion. 
+   # Use case? Attributes? Methods?
+   # def ...
 
 # Activity: Create another instance and call method based on class discussion
 t = Location(2, 1, "Tappan Square")
 
 # Activity: Print to see if object is working as intended
-print("TODO")
+print(t)
+print(t.x, t.y, t.name, t.box, t.ratings)
