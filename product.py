a65f41bd6f3c9b8ccd328c88c632391b2df3e468
Emily Q. Wang <ewang1@oberlin.edu>
2025-04-10 20:38:11 -0400

    progress on coffeeshop product inheritance subclasses

--- product.py
+++ product.py
@@ -12,3 +12,31 @@ class Product:
   def set_price(self, new_p):
     self.price = new_p
     return
+
+class Tea(Product):
+  def __init__(self, n, p, c):
+    super().__init__(n, p)
+    self.caffeine_level = c
+
+# class Ceramics(Product):
+#   pass
+
+class Fruit(Product):
+  def __init__(self, n, p, q):
+    super().__init__(n, p)
+    self.quantity = q
+  
+  def get_quantity(self):
+    return self.quantity
+  
+  def new_quantity(self, new_q):
+    self.quantity = new_q
+
+class Cookie(Product):
+  def __init__(self, n, p, s, a):
+    super().__init__(n, p)
+    self.stock = s
+    self.allergen = a
+  
+  def show_allergen(self):
+    return self.allergen
