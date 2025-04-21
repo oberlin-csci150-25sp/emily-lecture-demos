a8c612870a6480dc93ddee0edf0866ff57d85409
Emily Q. Wang <ewang1@oberlin.edu>
2025-04-21 11:47:23 -0400

    very green coding demo complete

--- verygreen.py
+++ verygreen.py
@@ -5,19 +5,18 @@ def circles(x, y, radius):
     draws a set of color-changing circles nested inside each other. Each run of circles() draws one circle with radius, cnetred at x and y
     '''
 
-    # TODO base case
+    # base case
     if radius < 10:
       # draw filled circle at x, y
-      pass
+      return
     
     # starter code for changing color and drawing filled circle at current values
     picture.set_fill_color(0, radius % 255, 0)
-    print(radius % 255)
     picture.draw_filled_circle(x, y, radius)
 
-    # TODO recursive call
+    # recursive call
     # draw filled circle at x, y
-    circles(x, y, radius - 3)
+    circles(x, y, radius - 10)
 
 def main():
   # set up a new canvas
@@ -33,9 +32,9 @@ def main():
 
   # TODO: one test circle before we try recursion!
   picture.set_fill_color(0, 255, 0)
-  picture.draw_filled_circle(x_center, y_center, dim // 3)
+  # picture.draw_filled_circle(x_center, y_center, dim // 3)
 
-  # circles(x_center, y_center, dim // 3)
+  circles(x_center, y_center, dim // 3)
 
   # save picture (show in different tab of codespace)
   picture.save_picture("verygreen.png")
